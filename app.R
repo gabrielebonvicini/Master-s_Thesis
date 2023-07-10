15442a3f9e529bfc82bb18fa9b1afc89ec5d2271
gabrielebonvicini <105913091+gabrielebonvicini@users.noreply.github.com>
2023-07-10 14:53:10 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,328 @@
+
+source("help.R")
+
+
+#### write here all the necessary functions and data #### 
+
+#p_rank_app <- read_xlsx("p_rank_app.xlsx")
+#####
+
+
+#Section 8: Shiny application -----------------------------------------------------
+
+##In this section a Shiny application to automatize the results will be created
+
+##8.1: UI ---------------------------------------------------------
+
+ui <- fluidPage(
+  ##1 Main and subtitle 
+  h2("Analyse transaction costs and returns"), #title
+  p("You can select the field and quartile of which you want to assess the transaction costs
+    and returns of a simple strategy. The strategy buy stocks at opening price
+    and sell them at closing price each day in the period from 2018 to end of 2021"), #subtitle
+  
+  ##2 Inputs definition
+  ##2.1 Input 1 -- > Quartile 
+  sidebarLayout(
+    selectInput(inputId = "quartile",
+                label = "Select the Quartile",
+                choices = c("lower", "middle", "upper", "comparison lower/upper")
+    ),
+    ##2.2 Input 2 -- > Pillar 
+    selectInput(
+      inputId = "pillar",
+      label = "Select the pillar",
+      choices = c("ENV", "GOV", "SOCIAL", "ESG")
+    )
+    ##End of sidebarLayout()
+  ),
+  
+  ##3 Outputs
+  mainPanel(
+    tableOutput(outputId = "Output_tab"), #table output
+    
+    plotOutput(outputId = "Output_plot") #plot output
+    ##End of mainPanel()
+  )
+  ##End of fluid page
+)
+
+##8.2: Server ----------------------------------------------------------------
+
+server <- function(input, output){ 
+  
+  ##1 Table Output (dataset definition)
+  output$Output_tab <- renderUI({
+    ##1.1 Create a different table if comaprison is chosen (comparison needs two tables)
+    if(input$quartile == "comparison lower/upper"){
+      ##1.2 Data.frames  
+      df1 <- trade_analysis(quartile = "lower", pillar = input$pillar)
+      df2 <- trade_analysis(quartile = "upper", pillar = input$pillar)
+      
+      ##1.3 Data.frame for the table (Upper quartile) 
+      table_upper <- data.frame(
+        c(
+          "Returns of the period" = paste0( "$ ", sum(df2$returns_dollar,na.rm = T) %>% round(digits = 2) ) ,
+          "Transaction costs (roundtrip costs)" = paste0( "$ ", sum(df2$tcost_dollar, na.rm = T) %>% round(digits = 2) ),
+          "Higher daily Return" = paste0( max(df2$returns_perce[,1]*100)%>% round(digits = 2), " %" ), 
+          "Lower daily Return" = paste0( min(df2$returns_perce[,1]*100) %>% round(digits = 2), " %")
+        )
+      )
+      
+      table_upper <- cbind(table_upper, c(
+        "", "", rownames( df2$returns_perce)[ which.max(df2$returns_perce[,1])], 
+        rownames( df2$returns_perce)[ which.min(df2$returns_perce[,1])]
+      ) )
+      colnames(table_upper) <- c( "Value" , "Date occurred" ) 
+      
+      ##1.4 Data.frame for the table (lower quartile)
+      table_lower <- data.frame(
+        c(
+          "Returns of the period" = paste0( "$ ", sum(df1$returns_dollar,na.rm = T) %>% round(digits = 2) ) ,
+          "Transaction costs (roundtrip costs)" = paste0( "$ ", sum(df1$tcost_dollar,na.rm = T) %>% round(digits = 2) ),
+          "Higher daily Return" = paste0( max(df1$returns_perce[,1]*100)%>% round(digits = 2), " %" ), 
+          "Lower daily Return" = paste0( min(df1$returns_perce[,1]*100) %>% round(digits = 2), " %")
+        )
+      )
+      
+      table_lower <- cbind(table_lower, c(
+        "", "", rownames( df1$returns_perce)[ which.max(df1$returns_perce[,1])], 
+        rownames( df1$returns_perce)[ which.min(df1$returns_perce[,1])]
+      ) )
+      colnames(table_lower) <- c( "Value" , "Date occcurred" )
+      
+      ##1.5 Upper + Lower 
+      table <- cbind(table_upper, table_lower)
+    } 
+    ##1.5 Else if not comparison ... one single dataframe should be created
+    else {
+      df <- trade_analysis(quartile = input$quartile, pillar = input$pillar)
+      table <- data.frame(
+        c(
+          "Returns of the period" = paste0( "$ ", sum(df$returns_dollar,na.rm = T) %>% round(digits = 2) ) ,
+          "Transaction costs (roundtrip costs)" = paste0( "$ ", sum(df$tcost_dollar,na.rm = T) %>% round(digits = 2) ),
+          "Higher daily Return" = paste0( max(df$returns_perce[,1]*100)%>% round(digits = 2), " %" ), 
+          "Lower daily Return" = paste0( min(df$returns_perce[,1]*100) %>% round(digits = 2), " %")
+        )
+      )
+      
+      table <- cbind(table, c(
+        "", "", rownames( df$returns_perce)[ which.max(df$returns_perce[,1])], 
+        rownames( df$returns_perce)[ which.min(df$returns_perce[,1])]
+      ) )
+      colnames(table) <- c( "Value" , "Date occcurred" )
+      ##End of else
+    }
+    
+    ##2 Table Output (table creation)
+    
+    ##2.1 If comaprison upper lower ... 
+    if(input$quartile == "comparison lower/upper"){
+      t <- table %>% 
+        kbl( caption = paste0("Period from ", rownames(df2$tcost_perce)[1], " to ", 
+                              rownames(df2$tcost_perce)[nrow(df2$tcost_perce)]) ) %>%
+        kable_styling(full_width = T, html_font = "Cambria", "striped")  %>%
+        add_header_above( c("." = 1, "Upper quartile" = 2, "Lower quartile" = 2 ) ) %>%
+        add_footnote(label = "The Upper quartile is the one with the stocks with more disagreement" )
+      ##End of if
+    } 
+    ##Else if not comparison upper/lower ...
+    else {
+      t <- table %>%
+        kbl( caption = paste0("Period from ", rownames(df$tcost_perce)[1], " to ", 
+                              rownames(df$tcost_perce)[nrow(df$tcost_perce)]) ) %>%
+        kable_styling(full_width = T, html_font = "Cambria", "striped") 
+      ##End of else
+    }
+    
+    return( HTML(as.character(t)) ) #transform the HTML into a table 
+    ##End of renderUI() (table output)
+  })
+  
+  ##3 Plot Output 
+  output$Output_plot <- renderPlot({
+    ##3.1 Again if comaprison upper/lower is selected, more plots are needed 
+    if(input$quartile == "comparison lower/upper"){
+      par(mfrow = c(2,2)) #four charts
+      
+      ##3.2 Image of the cumulated transaction costs and returns
+      df1 <- trade_analysis("lower" , pillar = input$pillar)
+      df2 <- trade_analysis("upper", pillar = input$pillar)
+      
+      ##3.3 Build the cumulative sum of returns
+      cumu_sum_ret <- numeric(length(df1$returns_perce[,1])) #empty element
+      for(i in 1:length(df1$returns_perce[,1])){   
+        if(i == 1){ #the first element
+          cumu_sum_ret[i] <- df1$returns_perce[,1][i]*100 
+          ##End of if 
+        } else { #The following elements  
+          cumu_sum_ret[i] <- cumu_sum_ret[i-1] + df1$returns_perce[,1][i]*100
+          ##End of else
+        }
+        ##End of loop
+      }
+      
+      ##3.4 The same for the upper quartile 
+      
+      ## Buidl the cumulative sum of returns
+      cumu_sum_ret2 <- numeric(length(df2$returns_perce[,1])) #now for returns
+      for(i in 1:length(df2$returns_perce[,1])){   
+        if(i == 1){ 
+          cumu_sum_ret2[i] <- df2$returns_perce[,1][i]*100 
+          ##End of if 
+        } else { 
+          cumu_sum_ret2[i] <- cumu_sum_ret2[i-1] + df2$returns_perce[,1][i]*100
+          ##End of else
+        }
+        ##End of loop
+      }
+      
+      ##3.5 Set the x-axis
+      ##Date axis 
+      date_axes <- as.Date( colnames(prices_day)[4:length(prices_day)], 
+                            tryFormats = "%d/%m/%Y" ) #the x_axes need to be of a date format
+      
+      ##3.6 Plot the lower quartile 
+      ##Scatter plot of transaction costs 
+      plot(df1$tcost_perce[,1]*100, x = date_axes, xaxt = "n" , yaxt = "n" , 
+           #xaxt = "n" -- > do not show x-axis
+           ylim = c(-10,10), cex = 0.3, pch = 20, #pch = 20 -- > fulld dots 
+           xlab =  "Dates", ylab = "percentage") 
+      ##Scatter plot of returns
+      points(y = df1$returns_perce[,1]*100, x = date_axes, col = "red", cex = 0.3, pch = 20)  
+      ##Legend
+      legend("topright", legend = c("Costs", "Returns"), col = c("black", "red"),pch = 20)
+      
+      ##3.7 Fix the axes 
+      ##The x-axix (1)
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") ,  
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      ##The y-xis (2) 
+      axis(2, at = seq(-10,10,by = 1), labels = F )  
+      axis(2, at = seq(-10,10,by = 2), labels = paste0(seq(-10,10,by = 2) , "%"), las = 1 ) 
+      #las = 1 -- > show horizontally                           
+      
+      ##3.8 Plot line of cumulated rets   
+      plot(x = date_axes, y = cumu_sum_ret, type = "l", xaxt = "n", yaxt = "n" ,  
+           ylim = c(-50,50), ylab = "percentage", xlab = "Dates") 
+      ##x-axis (1)
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") , 
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      ##Legend
+      legend("topright", legend = c( "Cumulative returns"), 
+             col = c("black"), lwd = 2)
+      
+      ##y-axis (2)
+      axis(2, at =  seq(-50,50, by = 10), labels = paste0( seq(-50,50, by = 10) , "%") , 
+           las = 1) 
+      
+      ##Add a main 
+      title( paste0(input$pillar, "  ", " lower quartile portfolio" ), 
+             outer = T, line = -1) #line = -1 -- > above the first two charts
+      
+      ##3.9 Do the same for the upper quartile
+      
+      ##Scatter plot of returns and transaction costs
+      ##Transaction costs
+      plot(df2$tcost_perce[,1]*100, x = date_axes, xaxt = "n" , yaxt = "n" ,  
+           ylim = c(-10,10), cex = 0.3, pch = 20,
+           xlab =  "Dates", ylab = "percentage" ) 
+      #Returns 
+      points(y = df2$returns_perce[,1]*100, x = date_axes, col = "red", cex = 0.3, pch = 20)
+      ##Legend
+      legend("topright", legend = c("Costs", "Returns"), col = c("black", "red"), pch = 20)
+      
+      #x-axis (1)
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") , 
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      ##y-axis (2)
+      axis(2, at = seq(-10,10,by = 1), labels = F )  
+      axis(2, at = seq(-10,10,by = 2), labels = paste0(seq(-10,10,by = 2) , "%"), las = 1 )
+      
+      ##Line showing cumulated returns 
+      ##Line chart
+      plot(x = date_axes, y = cumu_sum_ret2, type = "l", xaxt = "n", yaxt = "n" ,  
+           ylim = c(-50,50), ylab = "percentage", xlab = "Dates") #costs
+      ##x-axis 
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") , 
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      ##Legend
+      legend("topright", legend = c("Cumulative returns"), 
+             col = c("black"), lwd = 2)
+      
+      ##y-axis (2) 
+      axis(2, at =  seq(-50,50, by = 10), labels = paste0( seq(-50,50, by = 10) , "%") , 
+           las = 1) #to write horizontally  
+      
+      ##Add a title  
+      title( paste0(input$pillar, " ", " upper quartile portfolio" ), 
+             outer = T, line = -18) #line = -18 -- > In the middle of the four charts
+      ##End of if (comparison == T)
+    } 
+    ##3.10 Else (if input$quartile != comparison -- > two plots are needed instead of four)
+    else  { 
+      par(mfrow = c(1,2)) #two charts 
+      df <- trade_analysis(quartile = input$quartile, pillar = input$pillar)
+      
+      ##Cumulated returns
+      cumu_sum_ret <- numeric(length(df$returns_perce[,1])) 
+      for(i in 1:length(df$returns_perce[,1])){   
+        if(i == 1){ 
+          cumu_sum_ret[i] <- df$returns_perce[,1][i]*100 
+          ##End of if 
+        } else { 
+          cumu_sum_ret[i] <- cumu_sum_ret[i-1] + df$returns_perce[,1][i]*100
+          ##End of else
+        }
+        ##End of loop
+      }
+      
+      ##Date axis  
+      date_axes <- as.Date( colnames(prices_day)[4:length(prices_day)], 
+                            tryFormats = "%d/%m/%Y" ) #the x_axes need to be of a date format
+      
+      ##Scatter plot of rets and transaction costs (percentage)
+      plot(df$tcost_perce[,1]*100, x = date_axes, xaxt = "n" , yaxt = "n" ,  
+           ylim = c(-10,10), cex = 0.3, pch = 20, #costs scatter
+           xlab =  "Dates", ylab = "percentage" ) 
+      points(y = df$returns_perce[,1]*100, x = date_axes, col = "red", cex = 0.3, pch = 20) #returns 
+      legend("topright", legend = c("Costs", "Returns"), col = c("black", "red"),pch = 20)
+      ##Fix the axes
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") , 
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      axis(2, at = seq(-10,10,by = 1), labels = F )  
+      axis(2, at = seq(-10,10,by = 2), labels = paste0(seq(-10,10,by = 2) , "%"), las = 1 )
+      
+      ##Line cahrt with cumulated elements 
+      plot(x = date_axes, y = cumu_sum_ret, type = "l", xaxt = "n", yaxt = "n" ,  
+           ylim = c(-50,50), ylab = "percentage", xlab = "Dates") #rets
+      axis.Date(1, at = seq(date_axes[1], date_axes[length(date_axes)] , "6 months") , 
+                labels = T, format = "%m/%Y", tcl = -0.5) #add an axes with dates 
+      legend("topright", legend = c("Cumulative returns"), 
+             col = c("black"), lwd = 2)
+      axis(2, at = seq(-50,50, by = 10), labels = paste0( seq(-50,50, by = 10) , "%"), 
+           las = 1 ) #to write horizaontally 
+      
+      ##Main
+      title( paste0(input$pillar, input$quartile , " " ,"quartile portfolio") ,
+             outer = T,line = -1)
+      
+      ##End of else
+    }
+    ##End of renderPlot()
+  })
+  ##End of server()  
+}
+
+##8.3: Run the application --------------------------------------------------------
+
+shinyApp(ui = ui , server = server)
+
+
+
+#rsconnect::setAccountInfo(name='gabrielebonvicini',
+ #                         token='0AFD96FEDFC2500035BEE4B1EBBA3C96',
+  #                        secret='XVkz66uaTdlUTtZ5c7di3Ojw+H4azebd0JcQzq3c')
+
+#rsconnect::deployApp()
+
---
+++ help.R
@@ -0,0 +1,325 @@
+
+library(lubridate)
+library(readxl)
+library(purrr)
+library(quantmod)
+library(shiny) 
+library(kableExtra) #for tables
+library(knitr)      #for tables 
+library(moments)
+library(docstring)
+
+##Files 
+sd_ENV_app <- read_xlsx("sd_ENV.xlsx")
+sd_ESG_app <- read_xlsx("sd_ESG.xlsx")
+sd_SOCIAL_app <- read_xlsx("sd_SOCIAL.xlsx")
+sd_GOV_app <- read_xlsx("sd_GOV.xlsx")
+
+prices_open_month_app <- read_xlsx("prices_open_month_app.xlsx")
+prices_close_month_app <- read_xlsx("prices_close_month_app.xlsx")
+
+bid_month_app <- read_xlsx("bid_month_app.xlsx")
+ask_month_app <- read_xlsx("ask_month_app.xlsx")
+
+sub <- substr( colnames(bid_month_app), start = 1, stop =7 )
+colnames(bid_month_app) <- sub
+colnames(ask_month_app) <- sub
+colnames(prices_open_month_app) <- sub
+colnames(prices_close_month_app) <- sub
+
+#7 Quartile functions 
+stocks_upper_quartiles <- function(data, n = 6) {
+  
+  df <- as.data.frame( matrix(nrow = nrow(data), ncol = ncol(data)))
+  colnames(df) <- colnames(data)
+  for(r in 1:nrow(data)){
+    for(c in 7:ncol(data)){
+      if(!is.na(data[r,c]) && data[r,c] > quantile(data[,c], probs = 0.75, type  = 1, na.rm = T)[[1]]) {
+        df[r,c] <- data[r,1]
+      }
+    }
+  }
+  df <- df[ ,-c(1:n)]          
+  return(df)
+  #end of function
+}
+
+
+upper_quart_selector <- function(df){
+  ## Upper quartile selection #take just the name of the stocks
+  try <- stocks_upper_quartiles(df)
+  #and create a dataframe with in every column the name of the stocks in the best quartile 
+  df_upper_perce <- as.data.frame(matrix(nrow = nrow(try),ncol = ncol(try)))
+  colnames(df_upper_perce) <- colnames(try)
+  for(i in 1:length(try)){ 
+    if( length(try[!is.na(try[,i]),i]) == 0 ){ df_upper_perce[1:length(try[!is.na(try[,i]),i]),i] = NA }
+    else { 
+      df_upper_perce[1:length(try[!is.na(try[,i]),i]),i] <- cbind(
+        try[!is.na(try[,i]),i]
+      )
+    } #this is the dataframe with the first quartile in every column
+    #end of ifelse
+  } 
+  #end of loop
+  return(df_upper_perce)
+}
+
+
+stocks_lower_quartiles <- function(data, n = 6) {
+  
+  df <- as.data.frame( matrix(nrow = nrow(data), ncol = ncol(data)))
+  colnames(df) <- colnames(data)
+  for(r in 1:nrow(data)){
+    for(c in 7:ncol(data)){
+      if(!is.na(data[r,c]) && data[r,c] < quantile(data[,c], probs = 0.25, type  = 1, na.rm = T)[[1]]) {
+        df[r,c] <- data[r,1]
+      }
+    }
+  }
+  df <- df[ ,-c(1:n)]
+  return(df)
+}
+
+
+lower_quart_selector <- function(df){ 
+  try <- stocks_lower_quartiles(df)
+  #and create a dataframe with in every column the name of the stocks in the best quartile 
+  df_lower_perce <- as.data.frame(matrix(nrow = nrow(try),ncol = ncol(try)))
+  colnames(df_lower_perce) <- colnames(try)
+  for(i in 1:length(try)){ 
+    if( length(try[!is.na(try[,i]),i]) == 0 ){ df_lower_perce[1:length(try[!is.na(try[,i]),i]),i] = NA }
+    else { 
+      df_lower_perce[1:length(try[!is.na(try[,i]),i]),i] <- cbind(
+        try[!is.na(try[,i]),i]
+      )
+    } #this is the dataframe with the first quartile in every column
+    #end of ifelse
+  } 
+  #end of loop
+  return(df_lower_perce)
+  #end of function
+}
+
+stocks_middle_quartiles <- function(data, n = 6) {
+  
+  df <- as.data.frame( matrix(nrow = nrow(data), ncol = ncol(data)))
+  colnames(df) <- colnames(data)
+  for(r in 1:nrow(data)){
+    for(c in 7:ncol(data)){
+      if(!is.na(data[r,c]) && data[r,c] <= quantile(data[,c], probs = 0.75, type  = 1, na.rm = T)[[1]] &
+         !is.na(data[r,c]) && data[r,c] >= quantile(data[,c], probs = 0.25, type  = 1, na.rm = T)[[1]]){
+        df[r,c] <- data[r,1]
+      }
+    }
+  }
+  df <- df[ ,-c(1:n)]
+  return(df)
+  #end of function
+}
+
+
+middle_quart_selector <- function(df){ 
+  try <- stocks_middle_quartiles(df)
+  #and create a dataframe with in every column the name of the stocks in the best quartile 
+  df_middle_perce <- as.data.frame(matrix(nrow = nrow(try),ncol = ncol(try)))
+  colnames(df_middle_perce) <- colnames(try)
+  for(i in 1:length(try)){ 
+    if( length(try[!is.na(try[,i]),i]) == 0 ){ df_middle_perce[1:length(try[!is.na(try[,i]),i]),i] = NA }
+    else { 
+      df_middle_perce[1:length(try[!is.na(try[,i]),i]),i] <- cbind(
+        try[!is.na(try[,i]),i]
+      )
+    } #this is the dataframe with the first quartile in every column
+    #end of ifelse
+  } 
+  #end of loop
+  return(df_middle_perce)
+  #end of function 
+}
+
+
+
+
+
+trade_analysis <- function(
+    quartile = c("upper", "lower", "middle"), pillar = c("ESG", "ENV" , "SOCIAL", "GOV")
+){ 
+ 
+  ##1 If-else to decide the dataframe to use
+  ##Choose the pillar
+  if(pillar == "ESG"){ df <- sd_ESG_app } 
+  else if (pillar == "SOCIAL") { df <- sd_SOCIAL_app}
+  else if (pillar == "ENV"){ df <- sd_ENV_app} 
+  else if (pillar == "GOV") { df <- sd_GOV_app}
+  
+  ##Choose the quartile
+  if(quartile == "upper") { df <- upper_quart_selector(df)} 
+  else if (quartile == "lower"){ df <- lower_quart_selector(df)}
+  else if (quartile == "middle"){ df <- middle_quart_selector(df)}
+  
+  ##2 Dataframe construction 
+  quartile_port <- data.frame( #list of stocks to invest in
+    matrix(nrow = nrow(df), ncol = ncol(prices_open_month))
+  )
+  colnames(quartile_port) <- colnames(prices_open_month)
+  
+  ##Create the data.frame with the columns  
+  for(c in 4:ncol(quartile_port)){
+    quartile_port[ ,c] <- df[ ,colnames(df)==colnames(quartile_port[c])]
+  }
+  quartile_port <- quartile_port[ , -c(1:3)] #DON'T run again 
+  
+  ##3 Buy and Sell variables definition 
+  ##Empty elements
+  buy <- list()
+  sell <- list()
+  vec_of_stocks <- list()
+  
+  ##For loop to assign the stocks 
+  for(i in 1:length(quartile_port)){
+    vec_of_stocks[[i]] <- quartile_port[ ,i] 
+    buy[[i]] <- prices_open_month[which(prices_open_month$Stock %in% vec_of_stocks[[i]]),i+3]
+    sell[[i]] <- prices_close_month[which(prices_close_month$Stock %in% vec_of_stocks[[i]]),i+3]
+  }
+  
+  ##Transform them into a list object 
+  for(i in 1:length(buy)){
+    buy[i] <- as.list(buy[[i]][1])
+    sell[i] <- as.list(sell[[i]][1])
+  }
+  
+  ##4 Returns of the strategy 
+  ##Empty varaibles 
+  percentage <- buy
+  len <- numeric(length(buy))
+  total_percentage <- numeric(length = length(buy))
+  total_ret_money <- numeric(length(buy))
+  w_single <- numeric (length = length(buy))
+  
+  ## Compute the returns  
+  for(i in 1:length(buy)){
+    if (length(buy[[i]]) > 0){ #condition to skip NULL and NA elements in buy
+      
+      ##4.1 collect in one vector the lenght of the number of stocks in the quartile in each period
+      len[i] <- length(buy[[i]])
+      
+      ##4.2 You have the total wealth taht you will equally split among stocks 
+      w <- 100000
+      
+      ##4.3 Compute the returns(percentages) with a loop
+      for(l in 1:len[i]) {  #with 'l' changing every day 'i' and ranging for every stocks. 
+        #This is to account for the number of stocks changing every month
+        w_single[i] <- w / length(buy[[i]][[l]]) #total wealth / number of stocks in every period
+        percentage[[i]][l] <-  ( as.numeric(buy[[i]][[l]]) -  #opening price of stock 'l' in date 'i' 
+                                   as.numeric(sell[[i]][[l]]) ) / #closing price of stock 'l' in date 'i'
+          ( as.numeric(buy[[i]][[l]]) )  #obtain the percentage 
+        ##End of second loop
+      }
+      
+      ##4.4  Find the cumulated percentage for the day 'i' and multiply it for w_single to find the $ amount
+      total_percentage[i] <- sum( as.numeric( #cumulated percentage in day 'i'
+        percentage[[i]][[l]] )   #percentage in day 'i' of stock 'l' + perce day 'i' stock 'l+1' ....
+        , na.rm = T)
+      total_ret_money[i] <- total_percentage[i] * w_single[i]
+      ##4.5 compute the transaction costs (in another loop otherwise is too slow)
+      
+      ##End of if
+    } 
+    
+    ##Else if buy[[i]] has no observation -- > substitute with 0 and non NA (to not create problems)
+    else { 
+      percentage[[i]] <- 0
+      ##End of else
+    }
+    ##End of first loop
+  }
+  
+  ##5 Transaction costs of the strategy implementation
+  ##Empty elements
+  bid <- list()
+  ask <- list()
+  vec_of_stocks <- list()
+  
+  ##Assign the name of the stocks to bid and ask  
+  for(i in 1:length(quartile_port)){
+    vec_of_stocks[[i]] <- quartile_port[ ,i]
+    bid[[i]] <- bid_month[which(bid_month$Stock %in% vec_of_stocks[[i]]),i+3]
+    ask[[i]] <- ask_month[which(ask_month$Stock %in% vec_of_stocks[[i]]),i+3]
+  }
+  
+  ##Transform them into a list object 
+  for(i in 1:length(bid)){
+    bid[i] <- as.list(bid[[i]][1])
+    ask[i] <- as.list(ask[[i]][1])
+  }
+  
+  ##Empty elements 
+  tcost_percentage <- numeric(length(bid))
+  tcost_money <- numeric(length(bid))
+  percentage_cost <- bid  
+  len <- numeric( (length(bid)) )
+  w_single <- numeric(length = length(bid))
+  
+  ##Transaction costs of the portfolio 
+  for(i in 1:length(bid)){
+    if(length(bid[[i]])>0){ 
+      
+      ##5.1 collect in one vector the lenght of the number of stocks in the quartile in each period
+      len[i] <- length(bid[[i]])
+      
+      ##5.2 You have the total wealth taht you will equally split among stocks 
+      w <- 100000
+      
+      ##5.3 compute the transaction costs (percentage)
+      for(l in 1:len[i]) { 
+        w_single[i] <- w / length(bid[[i]]) #number of stocks in every period
+        percentage_cost[[i]][l] <-  ( as.numeric(ask[[i]][l]) -  #ask of stock 'l' in date 'i'  
+                                        as.numeric(bid[[i]][l]) ) / #bid of stock 'l' in date 'i'
+          ( ( as.numeric(bid[[i]][l]) + 
+                as.numeric(ask[[i]][l]) ) /2 ) #mid quote to obtain the %bid-ask
+        ##End of second loop
+      }
+      
+      ##5.4 Multiply the wealth invested in every stock times the %Bid-Ask spread
+      tcost_percentage[i] <- sum ( as.numeric(
+        percentage_cost[[i]] ) , na.rm = T
+      ) 
+      tcost_money[i] <- tcost_percentage[i] * w_single[i]
+      ##End of first loop 
+    }
+    
+    ##Else if buy[[i]] has no observation -- > substitute with 0 and non NA (to not create problems)
+    else {
+      percentage_cost[[i]] <- 0
+      ##End of else
+    }
+    ##End of if 
+  } 
+  
+  ##6 Final dataset creation 
+  ##Transaction costs in Percentage
+  tcost_percentage <- as.data.frame( tcost_percentage ) #in data.frame format
+  rownames(tcost_percentage) <- colnames( prices_day )[4:ncol(prices_day)] #rownames 
+  colnames(tcost_percentage) <- "Cost paid per day (percentage)" #column name
+  
+  ##Transaction costs in Dollar 
+  tcost_money <- as.data.frame( tcost_money )
+  rownames(tcost_money) <- colnames( prices_day )[4:ncol(prices_day)]
+  colnames(tcost_money) <- "Cost paid per day (dollar)"
+  
+  ##Returns in percentage
+  total_percentage <- as.data.frame( total_percentage )
+  rownames(total_percentage) <- colnames( prices_day )[4:ncol(prices_day)]
+  colnames(total_percentage) <- "Return per day (percentage)"
+  
+  ##Returns in dollar
+  total_ret_money <- as.data.frame( total_ret_money )
+  rownames(total_ret_money) <- colnames( prices_day )[4:ncol(prices_day)]
+  colnames(total_ret_money) <- "Cost paid per day (dollar)"
+  
+  ##7 Return a list of dataframes 
+  return( list (tcost_percentage = tcost_percentage, tcost_dollar = tcost_money, 
+                returns_perce = total_percentage, returns_dollar = total_ret_money) )
+  ##End of function 
+}
+
